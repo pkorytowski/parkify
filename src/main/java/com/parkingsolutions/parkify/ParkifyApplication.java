660ee63012474892edac52edd490edbc3fd6b439
Pawel Korytowski <korytows@student.agh.edu.pl>
2022-04-29 22:48:20 +0200

    Initial commit

---
+++ src/main/java/com/parkingsolutions/parkify/ParkifyApplication.java
@@ -0,0 +1,13 @@
+package com.parkingsolutions.parkify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkingsolutions/parkify/ParkifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkingsolutions.parkify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
